0d73244bb36ab37341fd22ff28704b1d6b8d7d76
rioforce <email>
2013-06-01 15:53:33 -0400

    6-1-13 commit 3

---
+++ Adventures in LEGO Hospital/Scripts/Screenshot.py
@@ -0,0 +1,5 @@
+import Rasterizer
+
+Rasterizer.makeScreenshot("Hospital#.tga")
+
+#Script from http://blenderartists.org/forum/showthread.php?79654-How-do-I-take-a-game-engine-screenshot&p=725210&viewfull=1#post725210
--- Adventures in LEGO Hospital/Scripts/cursor.py
+++
@@ -1,4 +0,0 @@
-import Rasterizer
-Rasterizer.showMouse(1) 
-
-# Script from http://www.blender.org/forum/viewtopic.php?t=352&sid=7f358240b210dc5a74e3e959f254c6f9
