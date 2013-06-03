0c33b8216b140e0d112f84f021421de82aabce17
rioforce <email>
2013-06-03 08:59:11 -0400

    Modeled most of Ambulance
    And testing out new Screenshot script by le717.

--- Adventures in LEGO Hospital/Scripts/Screenshot.py
+++ Adventures in LEGO Hospital/Scripts/Screenshot.py
@@ -1,5 +1,9 @@
 import Rasterizer
 
-Rasterizer.makeScreenshot("Hospital#.tga")
+i = 1
+
+i += 1
+Rasterizer.makeScreenshot("Hospital_{0}.png".format(str(i).zfill(10)))
+#i += 1
 
 #Script from http://blenderartists.org/forum/showthread.php?79654-How-do-I-take-a-game-engine-screenshot&p=725210&viewfull=1#post725210
