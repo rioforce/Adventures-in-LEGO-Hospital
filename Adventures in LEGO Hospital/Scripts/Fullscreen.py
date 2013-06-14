f2af489be29464a5a713c5ba7733ee503d830159
rioforce <email>
2013-06-14 13:18:29 -0400

    Worked on Fullscreen script

--- Adventures in LEGO Hospital/Scripts/Fullscreen.py
+++ Adventures in LEGO Hospital/Scripts/Fullscreen.py
@@ -1,36 +1,12 @@
 """ 
-Toggle between Fullscreen and Windowed mode
+Activates Fullscreen mode
 Written by Triangle717
 <http://Triangle717.WordPress.com/>
-"""
-
-"""The Issues:
-* Once either block is finished, it promptly runs the other block, thus not performing the desired actiom
-* I can get it to work only while the hotkey is held-down, not the way it needs to be
-* When fullscreen is activated, it loads in 1080p (as it is says below), but it needs to open in the screen's current resolution.
-However, I can't use bpy.types.SceneGameData.use_desktop as the bpy module is not compiled into a standalone runtime, and there is no
-bge.render version of it (though there should be).
 """
     
 import bge
 
-def main():
-    if bge.render.getFullScreen():
-        windowed()
-    elif not bge.render.getFullScreen(): 
-        fullscreen()
-    
-
-# It is full screen, kick to windowed mode
-def windowed():
-    bge.render.setFullScreen(False)
-    bge.render.setWindowSize(960, 540)
-    #raise SystemExit
-
-# It is windowed mode, kick to full screen 
-def fullscreen(): 
-    bge.render.setFullScreen(True)
-#    bge.render.setWindowSize(1920, 1080)   
-    #raise SystemExit
-    
-main()
+# Sets Fullscreen mode
+bge.render.setFullScreen(True)
+# No need to set window size, as
+#  "Use Desktop" switch in Blender is checked
